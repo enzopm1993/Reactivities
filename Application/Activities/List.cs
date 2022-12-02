30ac3d64c93715b0979b8a007fead0508501acd9
enzopm1993 <41841814+enzopm1993@users.noreply.github.com>
2022-12-02 00:25:15 -0500

    End of section 10

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,3 +1,4 @@
+using Application.Core;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -7,9 +8,9 @@ namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<List<Activity>> {}
+        public class Query : IRequest<Result<List<Activity>>> {}
 
-        public class Handler : IRequestHandler<Query, List<Activity>>
+        public class Handler : IRequestHandler<Query, Result<List<Activity>>>
         {
             private readonly DataContext _context;
 
@@ -19,22 +20,9 @@ namespace Application.Activities
                 _context = context;
             }
 
-            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<Result<List<Activity>>> Handle(Query request, CancellationToken cancellationToken)
             {
-                // try
-                // {
-                //     for(var i = 0; i < 10; i++)
-                //     {
-                //         cancellationToken.ThrowIfCancellationRequested();
-                //         await Task.Delay(1000, cancellationToken);
-                //         _logger.LogInformation($"Task {i} has completed");
-                //     }
-                // } catch(Exception ex) when (ex is TaskCanceledException)
-                // {
-                //     _logger.LogInformation("Task was cancelled");
-                // }
-
-                return await _context.Activities.ToListAsync(cancellationToken);
+                return Result<List<Activity>>.Sucess(await _context.Activities.ToListAsync(cancellationToken));
             }
         }
     }
